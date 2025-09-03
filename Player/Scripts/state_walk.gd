5db47e42eccfebb710a62a6a2f0a1d5cc9658860
huxiaoou <516984451@qq.com>
2025-09-03 22:24:17 +0800

    E04 accomplished

--- Player/Scripts/state_walk.gd
+++ Player/Scripts/state_walk.gd
@@ -11,7 +11,7 @@ func enter() -> void:
 func process(_delta: float) -> State:
 	if player.direction == Vector2.ZERO:
 		return idle
-	player.velocity = player.direction.normalized() * move_speed
+	player.velocity = player.direction * move_speed
 	if player.set_direction():
 		player.update_animation("walk")
 	return null
