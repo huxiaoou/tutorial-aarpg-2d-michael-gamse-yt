dab9b6efc599940154330ed8826605323b6a8cce
huxiaoou <516984451@qq.com>
2025-09-07 21:53:11 +0800

    E09 Accomplished

--- Enemies/Scripts/enemy_state_machine.gd
+++ Enemies/Scripts/enemy_state_machine.gd
@@ -1,35 +1,37 @@
-class_name PlayerStateMachine extends Node
+class_name EnemyStateMachine extends Node
 
-var states: Array[State]
-var prev_state: State
-var curr_state: State
+var states: Array[EnemyState]
+var prev_state: EnemyState
+var curr_state: EnemyState
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	process_mode = Node.PROCESS_MODE_DISABLED
-
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	change_state(curr_state.process(delta))
-
+	
 func _physics_process(delta: float) -> void:
 	change_state(curr_state.physics(delta))
 
-func _unhandled_input(event: InputEvent) -> void:
-	change_state(curr_state.handle_input(event))
-
-func initialize(_player: Player) -> void:
+func initialize(_enemy: Enemy) -> void:
 	states = []
 	for c in get_children():
-		if c is State:
+		if c is EnemyState:
 			states.append(c)
-	if states.size() > 0:
-		states[0].player = _player
+	for s in states:
+		s.enemy = _enemy
+		s.state_machine = self
+		s.init()
+
+	if not states.is_empty():
 		change_state(states[0])
 		process_mode = Node.PROCESS_MODE_INHERIT
 
-func change_state(new_state: State) -> void:
+
+func change_state(new_state: EnemyState) -> void:
 	if new_state == null || new_state == curr_state:
 		return
 	if curr_state:
